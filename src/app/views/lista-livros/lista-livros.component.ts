8211623571d0f7fa6cbef5e820258fdf839d9560
Lucas <lucasdec88@gmail.com>
2023-07-11 09:32:15 +0100

    Unsubscribe to avoid memory leak

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { BookService } from 'src/app/service/book.service';
 
 @Component({
@@ -6,17 +7,26 @@ import { BookService } from 'src/app/service/book.service';
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css'],
 })
-export class ListaLivrosComponent {
+export class ListaLivrosComponent implements OnDestroy {
+
   listaLivros: [];
   searchField: string = '';
+  subscription: Subscription;
 
   constructor(private bookService: BookService) {}
 
+  ngOnDestroy(): void {
+    //release resource to avoid memory leak
+    this.subscription.unsubscribe();
+  }
+
   searchBooks() {
-    this.bookService.search(this.searchField).subscribe({
+    this.subscription = this.bookService.search(this.searchField).subscribe({
       next: dataReturned => console.log(dataReturned), //success case. It returns values (string, number, object, etc).
       error: err => console.log(err),
       complete: () => console.log("'Complete' does not run in error case! It stops in error case.")
     });
+
+
   }
 }
