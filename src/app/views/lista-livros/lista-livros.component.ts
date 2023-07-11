c7de999ce2af9a27e4268ffab8e479c112fa1903
Lucas <lucasdec88@gmail.com>
2023-07-11 09:27:15 +0100

    Observable Life Cycle

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -13,14 +13,10 @@ export class ListaLivrosComponent {
   constructor(private bookService: BookService) {}
 
   searchBooks() {
-    this.bookService.search(this.searchField).subscribe(
-      (data) => {
-        console.log(data);
-      },
-      (error) => {
-        console.log(error);
-      },
-      () => {}
-    );
+    this.bookService.search(this.searchField).subscribe({
+      next: dataReturned => console.log(dataReturned), //success case. It returns values (string, number, object, etc).
+      error: err => console.log(err),
+      complete: () => console.log("'Complete' does not run in error case! It stops in error case.")
+    });
   }
 }
