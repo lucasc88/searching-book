08ab9d01df28345e357a91d0a7f9f4a7cbdd41a1
Lucas <lucasdec88@gmail.com>
2023-07-20 12:27:16 +0100

    Map Operator

--- src/app/service/book.service.ts
+++ src/app/service/book.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
-import { BookResult } from '../models/BookInterfaces';
+import { Observable, map, tap } from 'rxjs';
+import { BookResult, Item } from '../models/BookInterfaces';
 
 @Injectable({
   providedIn: 'root',
@@ -12,12 +12,14 @@ export class BookService {
 
   constructor(private http: HttpClient) {}
 
-  search(value: string): Observable<BookResult> {
+  search(value: string): Observable<Item[]> {
     const params = new HttpParams().append('q', value);
     return this.http.get<BookResult>(this.API, { params })
     .pipe(//pipe aloows to use any other operator
       //tap does not change the previous operator flow, it's just a simple operator
-      tap(returnedData => console.log("returnedData: " + JSON.stringify(returnedData)))
+      tap(returnedData => console.log("returnedData: " + JSON.stringify(returnedData))),
+      //mapping from BookResult to Item[]. Map transforms the object into other one.
+      map(result => result.items)
     );
   }
 }
